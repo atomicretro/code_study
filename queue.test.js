25fd6607dc33f5ab72ff0aaa5c5fa2541dd94597
alec cuccia <ajcuccia@gmail.com>
2018-08-19 13:45:35 -0400

    Refactor queue.test.js and stack.test.js slightly

--- queue.test.js
+++ queue.test.js
@@ -102,9 +102,10 @@ describe('initialize Queue without a starting element', () => {
     expect(queue.dequeue()).toBeUndefined();
   });
 
-  test('Queue#dequeue() returns correct element on non-empty queue', () => {
+  test('Queue#dequeue() removes and returns correct element on non-empty queue', () => {
     queue.enqueue(1);
     expect(queue.dequeue()).toBe(1);
+    expect(queue._queue[0]).toBeUndefined();
   });
 
   test('Queue#length() returns correct length *after* Queue#dequeue', () => {
@@ -144,119 +145,19 @@ describe('initialize Queue with a starting element', () => {
     expect(queue._queue[0]).toBe(1);
   });
 
+  test('Queue#peek() without argument returns initialized element', () => {
+    expect(queue.peek()).toBe(1);
+  });
+
+  test('Queue#dequeue() removes and returns initialized element', () => {
+    expect(queue.dequeue()).toBe(1);
+    expect(queue._queue[0]).toBeUndefined();
+  });
+
   test('Queue#enqueue(el) adds element to back of queue', () => {
-    expect(queue._queue[0]).toBe(1);
     queue.enqueue(2);
     expect(queue._queue[1]).toBe(2);
     queue.enqueue(3);
     expect(queue._queue[2]).toBe(3);
-  });
-
-  test('Queue has no trouble enqueueing equivalent elements', () => {
-    expect(queue._queue[0]).toBe(1);
-    queue.enqueue(1);
-    expect(queue._queue[1]).toBe(1);
-    queue.enqueue(1);
-    expect(queue._queue[2]).toBe(1);
-  });
-
-  test('Queue#peek() returns undefined on empty queue', () => {
-    queue.dequeue()
-    expect(queue.peek()).toBeUndefined();
-    expect(queue.peek(1)).toBeUndefined();
-  });
-
-  test('Queue#peek() without argument returns frontmost element', () => {
-    expect(queue.peek()).toBe(1);
-    queue.enqueue(2);
-    expect(queue.peek()).toBe(1);
-  });
-
-  test('Queue#peek with valid argument returns nth most element from front', () => {
-    expect(queue.peek(0)).toBe(1);
-    queue.enqueue(2);
-    queue.enqueue(3);
-    queue.enqueue(4);
-    expect(queue.peek(3)).toBe(4);
-  });
-
-  test('Queue#peek with invalid argument returns frontmost most element', () => {
-    expect(queue.peek('a')).toBe(1);
-    queue.enqueue(2);
-    queue.enqueue(3);
-    queue.enqueue(4);
-    expect(queue.peek(null)).toBe(1);
-  });
-
-  test('Queue#peekBack() returns undefined on empty queue', () => {
-    queue.dequeue()
-    expect(queue.peekBack()).toBeUndefined();
-    expect(queue.peekBack(1)).toBeUndefined();
-  });
-
-  test('Queue#peekBack() without argument returns backmost element', () => {
-    expect(queue.peekBack()).toBe(1);
-    queue.enqueue(2);
-    expect(queue.peekBack()).toBe(2);
-  });
-
-  test(
-    'Queue#peekBack(n) with valid argument returns nth most element from back',
-    () => {
-      expect(queue.peekBack(0)).toBe(1);
-      queue.enqueue(2);
-      queue.enqueue(3);
-      expect(queue.peekBack(2)).toBe(1);
-  });
-
-  test(
-    'Queue#peekBack(n) with invalid argument returns nth most element from back',
-    () => {
-      expect(queue.peekBack('a')).toBe(1);
-      queue.enqueue(2);
-      queue.enqueue(3);
-      expect(queue.peekBack(null)).toBe(3);
-  });
-
-  test('Queue#length() returns correct length of queue', () => {
-    expect(queue.length()).toBe(1);
-    queue.enqueue(2);
-    expect(queue.length()).toBe(2);
-    queue.enqueue(3);
-    expect(queue.length()).toBe(3);
-  });
-
-  test('Queue#dequeue() returns undefined on empty queue', () => {
-    queue.dequeue()
-    expect(queue.dequeue()).toBeUndefined();
-  });
-
-  test('Queue#dequeue() returns correct element on non-empty queue', () => {
-    expect(queue.dequeue()).toBe(1);
-  });
-
-  test('Queue#length() returns correct length *after* Queue#dequeue', () => {
-    queue.enqueue(2);
-    queue.dequeue();
-    expect(queue.length()).toBe(1);
-  });
-
-  test('Queue integrity remains through 100,000 random operations', () => {
-    let internalQueueLength = 0;
-    let queueBack = 1;
-    for(let i = 0; i < 100000; i++) {
-      if(Math.random() < 0.5) {
-        queue.enqueue(i);
-        queueBack++;
-      } else {
-        queue.dequeue();
-      };
-    };
-    for(key in queue._queue) internalQueueLength++;
-
-    expect(queue._back).toBe(queueBack);
-    expect(queue.length()).toBe(internalQueueLength);
-    expect(queue.peek()).toBe(queue._queue[queueBack - internalQueueLength]);
-    expect(queue.peekBack()).toBe(queue._queue[queueBack - 1]);
   });
 });
--- stack.test.js
+++ stack.test.js
@@ -45,9 +45,10 @@ describe('initialize Stack without a starting element', () => {
     expect(stack.pop()).toBeUndefined();
   });
 
-  test('Stack#pop() returns correct element on non-empty stack', () => {
+  test('Stack#pop() removes and returns correct element on non-empty stack', () => {
     stack.push(1);
     expect(stack.pop()).toBe(1);
+    expect(stack._stack[0]).toBeUndefined();
   });
 
   test('Stack#length() returns correct length *after* Stack#pop()', () => {
@@ -80,8 +81,17 @@ describe('initialize Stack with a starting element', () => {
     stack = new Stack(1);
   });
 
-  test('Stack initializes with one element', () => {
+  test('Stack initializes with starting element', () => {
     expect(stack._stack[0]).toBe(1);
+  });
+
+  test('Stack#peek() returns initialized element', () => {
+    expect(stack.peek()).toBe(1);
+  });
+
+  test('Stack#pop() removes and returns initialized element', () => {
+    expect(stack.pop()).toBe(1);
+    expect(stack._stack[0]).toBeUndefined();
   });
 
   test('Stack#push(el) adds element to top of stack', () => {
@@ -89,61 +99,5 @@ describe('initialize Stack with a starting element', () => {
     expect(stack._stack[1]).toBe(2);
     stack.push(3);
     expect(stack._stack[2]).toBe(3);
-  });
-
-  test('Stack has no trouble pushing on equivalent elements', () => {
-    expect(stack._stack[0]).toBe(1);
-    stack.push(1);
-    expect(stack._stack[1]).toBe(1);
-    stack.push(1);
-    expect(stack._stack[2]).toBe(1);
-  })
-
-  test('Stack#peek() returns topmost element', () => {
-    expect(stack.peek()).toBe(1);
-    stack.push(2);
-    expect(stack.peek()).toBe(2);
-    stack.push(3);
-    expect(stack.peek()).not.toBe(2);
-  });
-
-  test('Stack#length() returns correct length of stack', () => {
-    expect(stack.length()).toBe(1);
-    stack.push(2);
-    expect(stack.length()).toBe(2);
-    stack.push(3);
-    expect(stack.length()).toBe(3);
-  });
-
-  test('Stack#pop() returns undefined on empty stack', () => {
-    stack.pop();
-    expect(stack.pop()).toBeUndefined();
-  });
-
-  test('Stack#pop() returns correct element on non-empty stack', () => {
-    stack.push(2);
-    expect(stack.pop()).toBe(2);
-  });
-
-  test('Stack#length() returns correct length *after* Stack#pop()', () => {
-    stack.push(2);
-    stack.pop();
-    expect(stack.length()).toBe(1);
-  });
-
-  test('Stack integrity remains through 100,000 random operations', () => {
-    for(let i = 0; i < 100000; i++) {
-      if(Math.random() < 0.5) stack.push(i);
-      else stack.pop();
-    };
-
-    let internalStackLength = 0;
-    for(key in stack._stack) {
-      expect(parseInt(key)).toBeGreaterThanOrEqual(0);
-      internalStackLength++;
-    };
-    expect(stack.length()).toBe(internalStackLength);
-    expect(stack._pointer).toBe(internalStackLength);
-    expect(stack.peek()).toBe(stack._stack[internalStackLength - 1]);
   });
 });
